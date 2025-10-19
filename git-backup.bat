f9959319f943974099c32ce742a6aa69be1fb5e0
drhye <drhye@example.com>
2025-10-19 23:21:11 +0900

    Backup: Before moving life-table-info div position

--- git-backup.bat
+++ git-backup.bat
@@ -2,6 +2,6 @@
 chcp 65001 > nul
 cd /d "%~dp0"
 git add -A
-git commit -m "Backup: Before moving life table info above tabs"
+git commit -m "Backup: Before moving life-table-info div position"
 git log --oneline -3
 
